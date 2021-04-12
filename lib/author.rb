19d652148f4d217719405df2362428af5e8ae5dd
Jacob W Jones <jacobwadejones76@gmail.com>
2021-04-12 18:52:23 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,32 +1,25 @@
 class Author
-  attr_accessor :title
-
+  attr_accessor :name
+  
   @@all = []
-
-  def initialize(title)
-    @name = title
+  
+  def initialize(name)
+    @name = name
     @@all << self
   end
-
+  
   def self.all
     @@all
   end
-
   
   def add_post(post)
     post.author = self
   end
-
   
   def add_post_by_title(title)
-    song = Post.new(title)
-    @posts << post
+    post = Post.new(title)
     post.author = self
-    @@sounds_count += 1
   end
-
- 
-  def self.post_count
-    Post.all.count
-  end
-end
+  
+  def posts
+    Post.all.select {|post| post.author == self}
